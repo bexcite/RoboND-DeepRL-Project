e2db0c35bd2f1c86b96cdba42519313e1d96b368
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2018-05-18 16:01:06 -0700

    total increase

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -688,7 +688,7 @@ void ArmPlugin::OnUpdate(const common::UpdateInfo& updateInfo)
 				// compute the smoothed moving average of the delta of the distance to the goal
 				avgGoalDelta  = avgGoalDelta * REWARD_ALPHA + distDelta * (1 - REWARD_ALPHA);
 				// rewardHistory = 2 * avgGoalDelta - 0.05; // this works for Task #1
-        rewardHistory = 4 * avgGoalDelta - 0.5; // Task #2 experiments
+        rewardHistory = 4 * avgGoalDelta - 1.5; // Task #2 experiments 0.2
 				newReward     = true;
 			}
 
