ca02604e16061c655d46d1d8a8b22757e0f3567e
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2018-05-18 17:07:58 -0700

    return reward back: prev:24/78

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -50,7 +50,7 @@
 /
 */
 
-#define REWARD_WIN  1000.0f // 40 works so so; 20 for task #1
+#define REWARD_WIN  100.0f // 40 works so so; 20 for task #1
 #define REWARD_LOSS -20.0f // -10.0f - forks for task 1
 
 #define REWARD_ALPHA 0.3f
