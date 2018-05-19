bf4262895c114c212d185b1045753cec1f50ea3b
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2018-05-19 11:45:55 -0700

    increase lstm size + batch size

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -38,11 +38,11 @@
 #define INPUT_WIDTH   64
 #define INPUT_HEIGHT  64
 #define OPTIMIZER "RMSprop"
-#define LEARNING_RATE 0.02f
+#define LEARNING_RATE 0.01f
 #define REPLAY_MEMORY 10000
-#define BATCH_SIZE 8
+#define BATCH_SIZE 32
 #define USE_LSTM true
-#define LSTM_SIZE 16
+#define LSTM_SIZE 256
 // #define LSTM_SIZE 32
 
 /*
