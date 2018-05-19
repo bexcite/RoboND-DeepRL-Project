8c52a103288a3adf58307d7059d4290a9bed3909
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2018-05-19 16:58:44 -0700

    some debug

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -733,7 +733,7 @@ void ArmPlugin::OnUpdate(const common::UpdateInfo& updateInfo)
 			avgGoalDelta     = 0.0f;
 
 			// track the number of wins and agent accuracy
-      printf("runs = %03u\n", totalRuns % GAME_HISTORY);
+      printf("runsId = %03u\n", totalRuns % GAME_HISTORY);
 			if( rewardHistory >= REWARD_WIN ) {
 				successfulGrabs++;
         history[totalRuns % GAME_HISTORY] = true;
@@ -744,11 +744,14 @@ void ArmPlugin::OnUpdate(const common::UpdateInfo& updateInfo)
       // count wins in the local history. Size of GAME_HISTORY
       int localAccuracyCnt = 0;
       int actualHistorySize = (totalRuns + 1) > GAME_HISTORY ? GAME_HISTORY : (totalRuns + 1);
+      printf("actualHistorySize = %03u\n", actualHistorySize);
       for (int i = 0; i < actualHistorySize; ++i) {
         if (history[i]) {
           localAccuracyCnt++;
+          printf("- BINGO");
         }
       }
+      printf("\n");
 
 			totalRuns++;
 			printf("Current Accuracy:  %0.4f (%03u of %03u)  (reward=%+0.2f %s)\n", float(successfulGrabs)/float(totalRuns), successfulGrabs, totalRuns, rewardHistory, (rewardHistory >= REWARD_WIN ? "WIN" : "LOSS"));
