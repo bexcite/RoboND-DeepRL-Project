1813595d107832642c24ae3021ab3cb0de105ec3
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2018-05-19 19:30:50 -0700

    batch_size back to 32

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -41,7 +41,7 @@
 // #define OPTIMIZER "Adam"
 #define LEARNING_RATE 0.1f
 #define REPLAY_MEMORY 10000
-#define BATCH_SIZE 64
+#define BATCH_SIZE 32
 #define USE_LSTM true
 #define LSTM_SIZE 256
 // #define LSTM_SIZE 32
